04dbda5bd0eb2a91363716a302202ab20b9f23d2
Prithivi Lakshmanan <prithivilaksh@gmail.com>
2025-08-25 18:40:26 +0530

    added client and server code

--- client/main.go
+++ client/main.go
@@ -1,1 +1,100 @@
-package client
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"math/rand/v2"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/prithivilaksh/streaming-cache/proto/cache"
+)
+
+// func get(cacheClient pb.CacheClient, tkr string) {
+// 	ctx := context.Background()
+// 	res, err := cacheClient.Get(ctx, &pb.Tkr{Tkr: tkr})
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println(res)
+// }
+
+func getStream(cacheClient pb.CacheClient) {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*40)
+	defer cancel()
+
+	stream, err := cacheClient.GetStream(ctx, &pb.Tkr{Tkr: "GOOGL"})
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(res)
+	}
+}
+
+// func set(cacheClient pb.CacheClient, tkrData *pb.TkrData) {
+
+// }
+
+func setStream(cacheClient pb.CacheClient) {
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*40)
+	defer cancel()
+
+	stream, err := cacheClient.SetStream(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		tkrData := &pb.TkrData{
+			Tkr:       "GOOGL",
+			Timestamp: time.Now().Unix(),
+			Price:     rand.Float64() * 100,
+			Volume:    rand.Int64(),
+		}
+		if err := stream.Send(tkrData); err != nil {
+			panic(err)
+		}
+		ack, err := stream.Recv()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(ack.Success)
+		time.Sleep(time.Duration(rand.Int32N(10)) * time.Second)
+	}
+}
+
+var serverAddr = flag.String("addr", "localhost:50051", "The server address in the format of host:port")
+
+func main() {
+	flag.Parse()
+
+	grpcClient, err := grpc.NewClient(*serverAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+
+	defer grpcClient.Close()
+
+	cacheClient := pb.NewCacheClient(grpcClient)
+
+	// get(cacheClient, "GOOGL")
+	getStream(cacheClient)
+	// set(cacheClient)
+	setStream(cacheClient)
+}
--- server/main.go
+++ server/main.go
@@ -4,21 +4,33 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"strconv"
 
 	pb "github.com/prithivilaksh/streaming-cache/proto/cache"
 	"github.com/redis/go-redis/v9"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func InitRedis() *redis.Client {
-	return redis.NewClient(&redis.Options{
+	rdb := redis.NewClient(&redis.Options{
 		Addr:     os.Getenv("REDIS_ADDR"),
 		Username: "default",
 		Password: os.Getenv("REDIS_PASSWORD"),
 		DB:       0,
 	})
+
+	err := rdb.Ping(context.Background()).Err()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return rdb
 }
 
 type cacheServer struct {
@@ -33,19 +45,124 @@ func NewCacheServer(rdb *redis.Client) *cacheServer {
 }
 
 func (s *cacheServer) Get(ctx context.Context, req *pb.Tkr) (*pb.TkrData, error) {
-	return &pb.TkrData{}, nil
+
+	pipe := s.rdb.TxPipeline()
+
+	cmd := pipe.HGetAll(ctx, req.Tkr)
+	_, err := pipe.Exec(ctx)
+
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "redis error: %v", err)
+	}
+
+	res, err := cmd.Result()
+	if err == redis.Nil {
+		return nil, status.Errorf(codes.NotFound, "key not found")
+	} else if err != nil {
+		return nil, status.Errorf(codes.Internal, "redis error: %v", err)
+	}
+
+	tkr := res["tkr"]
+	timestamp, _ := strconv.ParseInt(res["timestamp"], 10, 64)
+	price, _ := strconv.ParseFloat(res["price"], 64)
+	volume, _ := strconv.ParseInt(res["volume"], 10, 64)
+
+	tkrData := &pb.TkrData{
+		Tkr:       tkr,
+		Timestamp: timestamp,
+		Price:     price,
+		Volume:    volume,
+	}
+
+	return tkrData, nil
 }
 
 func (s *cacheServer) GetStream(req *pb.Tkr, stream pb.Cache_GetStreamServer) error {
-	return nil
+
+	ctx := stream.Context()
+	chnl := s.rdb.Subscribe(ctx, req.Tkr).Channel()
+
+	getAndSend := func() error {
+		res, err := s.Get(ctx, req)
+		if err != nil {
+			return err
+		}
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if err := getAndSend(); err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case <-ctx.Done(): // client cancelled or disconnected
+			return ctx.Err()
+
+		case _, ok := <-chnl:
+			if !ok { // subscription closed
+				return nil
+			}
+			// only send if actual Redis update comes in
+			if err := getAndSend(); err != nil {
+				return err
+			}
+		}
+	}
 }
 
 func (s *cacheServer) Set(ctx context.Context, req *pb.TkrData) (*pb.Ack, error) {
-	return &pb.Ack{}, nil
+
+	pipe := s.rdb.TxPipeline()
+
+	val := map[string]any{
+		"tkr":       req.Tkr,
+		"timestamp": req.Timestamp,
+		"price":     req.Price,
+		"volume":    req.Volume,
+	}
+
+	cmd1 := pipe.HSet(ctx, req.Tkr, val)
+	cmd2 := pipe.Publish(ctx, req.Tkr, "updated")
+	_, err := pipe.Exec(ctx)
+
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "redis error: %v", err)
+	}
+
+	if cmd1.Err() != nil {
+		return nil, status.Errorf(codes.Internal, "redis error: %v", cmd1.Err())
+	}
+
+	if cmd2.Err() != nil {
+		return nil, status.Errorf(codes.Internal, "redis error: %v", cmd2.Err())
+	}
+
+	return &pb.Ack{Success: true}, nil
 }
 
 func (s *cacheServer) SetStream(stream pb.Cache_SetStreamServer) error {
-	return nil
+
+	ctx := stream.Context()
+
+	for {
+		tkrData, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		if _, err := s.Set(ctx, tkrData); err != nil {
+			return err
+		}
+		if err := stream.Send(&pb.Ack{Success: true}); err != nil {
+			return err
+		}
+	}
 }
 
 var port = flag.String("port", "50051", "port to listen on")
@@ -61,20 +178,5 @@ func main() {
 	rdb := InitRedis()
 	pb.RegisterCacheServer(grpcServer, NewCacheServer(rdb))
 	grpcServer.Serve(lis)
-
-	// ctx := context.Background()
-
-	// err := rdb.Set(ctx, "foo", "bar", 0).Err()
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// result, err := rdb.Get(ctx, "foo").Result()
-
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// fmt.Println(result)
 
 }
